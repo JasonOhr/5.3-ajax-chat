88fbba5fc001d4e5e5329099ba051d7c3175a9db
Jason Ohr <jason.ohr@gmail.com>
2015-06-03 16:37:22 -0400

    I have can receive and render all the basic information from the server as well as post a message

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -3,8 +3,23 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     return "<h1>Hello</h1>\n";
 },"useData":true});
 this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>chat</h1>";
+    return "<h1>Hello</h1>\n<div class=\"messages\"></div>\n<form class=\"new-message-form\">\n    <input type=\"text\" class=\"newMessage\" placeholder=\"New Message\"/>\n    <input type=\"submit\" class=\"sendMessage\" value=\"Send\"/>\n</form>";
 },"useData":true});
 this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<form class=\"login-form\" >\n    <input type=\"text\" placeholder=\"Username\" class=\"login-user\"/>\n    <input type=\"submit\" placeholder=\"Login\"/>\n</form>";
+    return "<form class=\"login-form\" >\n    <input type=\"text\" placeholder=\"Username\" class=\"login-user\"/>\n    <input type=\"submit\" value=\"Login\"/>\n</form>";
+},"useData":true});
+this["JST"]["messages"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "\n<div><span>"
+    + alias3(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"username","hash":{},"data":data}) : helper)))
+    + "</span><span>"
+    + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
+    + "</span><span>"
+    + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
+    + "</span></div>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   var username = "";
-
+  var chatMessage = "";
   $(document).ready(function(){
     route();
     $(document).on('submit', '.login-form',function(event){
@@ -12,19 +12,57 @@
     $(window).on('hashchange',function(event){
       event.preventDefault();
       route();
-    })
+    });
+    $(document).on('submit',".new-message-form",function(e){
+      e.preventDefault();
+      postMessage()
+    });
 
   });
 
+
+  function getChatData(){
+    $.ajax({
+      url:"http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+      type: "GET"
+
+    }).then(function(messages) {
+      renderChat();
+      return messages
+    }).then(function(whatever){
+      console.log(whatever);
+      return whatever;
+    }).then(renderMessages);
+  }
+  function postMessage(){
+    chatMessage = $('.newMessage').val();
+    $.ajax({
+      url:"http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+      type: "POST",
+      data: {
+        "username": username,
+        "created_at": new Date().Date(),
+        "message": chatMessage
+      }
+    })
+
+  }
+  function renderMessages(messages){
+    $(".messages").html(JST['messages'](messages))
+  }
   function route(){
     switch (window.location.hash){
       case ''://in intital load
             $(".application").html(JST['login']());
             break;
       case '#/chat':
-            $('.application').html(JST['chat']());
+            getChatData();
             break;
     }
+  }
+  function renderChat(chatData){
+    $('.application').html(JST['chat'](chatData));
+    console.log("user: ",username);
   }
 
 })();
