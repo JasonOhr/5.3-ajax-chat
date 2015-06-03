d15fd04aa41fd8de16f75715c1c32497ffbb33cd
Jason Ohr <jason.ohr@gmail.com>
2015-06-03 13:35:33 -0400

    Initial commit

---
+++ dist/scripts/templates.js
@@ -0,0 +1,10 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Hello</h1>\n";
+},"useData":true});
+this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>chat</h1>";
+},"useData":true});
+this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<form class=\"login-form\" >\n    <input type=\"text\" placeholder=\"Username\" class=\"login-user\"/>\n    <input type=\"submit\" placeholder=\"Login\"/>\n</form>";
+},"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,30 @@
 (function(){
   'use strict';
+  var username = "";
 
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+    route();
+    $(document).on('submit', '.login-form',function(event){
+      event.preventDefault();
+      username = $(this).find('.login-user').val();
+      window.location.hash = '/chat';
+    });
+    $(window).on('hashchange',function(event){
+      event.preventDefault();
+      route();
+    })
+
   });
+
+  function route(){
+    switch (window.location.hash){
+      case ''://in intital load
+            $(".application").html(JST['login']());
+            break;
+      case '#/chat':
+            $('.application').html(JST['chat']());
+            break;
+    }
+  }
+
 })();
